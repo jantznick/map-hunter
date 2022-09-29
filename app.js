7241ea69f7a795ac5a571557d1f280b85e2e6601
Nick Jantz <nick@Rachels-MacBook-Air.local>
2022-09-29 15:20:29 -0500

    add ability to add markers to map and draw lines between them

--- app.js
+++ app.js
@@ -15,6 +15,24 @@ app.get('/geocode', (req, res) => {
     });
 })
 
+app.get('/nearest-road', (req, res) => {
+    // send lat and long get nearest road lat and long like:
+    // {"latitude":41.76916258266522,"longitude":-87.94925735613717}
+    request(`https://roads.googleapis.com/v1/nearestRoads?points=${req.query.lat},${req.query.long}&key=${googleMapsApiKey}`, (request, response, body) => {
+        console.log(JSON.parse(body))
+        res.send(JSON.parse(body).snappedPoints ? JSON.parse(body).snappedPoints[0].location : req.query)
+    });
+})
+
+app.get('/directions', (req, res) => {
+    const {
+        fromLat, fromLong, toLat, toLong
+    } = req.query;
+    request(`https://maps.googleapis.com/maps/api/directions/json?mode=WALKING&origin=${fromLat},${fromLong}&destination=${toLat},${toLong}&key=${googleMapsApiKey}`, (request, response, body) => {
+        res.send(JSON.parse(body).routes[0].legs)
+    })
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
